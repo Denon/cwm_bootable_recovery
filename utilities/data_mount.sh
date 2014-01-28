4ca37f9f4a5010ede15d03d0cde787fb82c7a50f
wimpknocker <jarkko.koistinen@hotmail.fi>
2014-01-28 12:21:32 +0200

    v1.1 secondrom sdcard

--- utilities/data_mount.sh
+++ utilities/data_mount.sh
@@ -4,7 +4,7 @@ BB="busybox"
 FILESYSTEM=$1
 
 
-BLOCKDEVICE=mmcblk0p25
+BLOCKDEVICE=mmcblk1p1
 
 if ! $BB grep -q /.secondrom /proc/mounts ; then
 mount -t ext4 /dev/block/$BLOCKDEVICE /.secondrom || exit 1
@@ -12,19 +12,20 @@ fi
 
 if ! $BB grep -q /data /proc/mounts ; then
 if $BB grep -q secondary /etc/.firstrundone; then # secondary
-        mkdir -p /.secondrom/media/.secondrom/data
+        mkdir -p /.secondrom/data/media
         mkdir -p /data
-        mount --bind /.secondrom/media/.secondrom/data /data
+        mount -r /.secondrom/data /data
+        
+   if ! $BB grep -q /data/media /proc/mounts ; then
+        mkdir -p /data/media
+        mount -r /.secondrom/data/media /data/media
+        fi
 
-   elif $BB grep -q primary /etc/.firstrundone; then # primary
+ elif $BB grep -q primary /etc/.firstrundone; then # primary
         mkdir -p /data
-        mount --bind /.secondrom /data
+        mount -t ext4 /dev/block/mmcblk0p25 /data || exit 1
    fi
 fi
 
-if ! $BB grep -q /data/media /proc/mounts ; then
-mkdir -p /data/media
-mount --bind /.secondrom/media /data/media || exit 1
-fi
 
 exit 0
