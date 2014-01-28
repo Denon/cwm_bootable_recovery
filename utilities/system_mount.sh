61367e789b1638bb66282729e34ce41ade1cfbb5
wimpknocker <jarkko.koistinen@hotmail.fi>
2014-01-28 12:27:37 +0200

    v1.1 secondrom sdcard

--- utilities/system_mount.sh
+++ utilities/system_mount.sh
@@ -1,12 +1,12 @@
 BB="busybox"
 FILESYSTEM=$1
-BLOCKDEVICE=mmcblk0p25
+BLOCKDEVICE=mmcblk1p1
 
 
 if $BB [ "$FILESYSTEM" == "secondary" ]; then
 $BB mkdir -p /.secondrom
 $BB mount -t ext4 /dev/block/$BLOCKDEVICE /.secondrom
-$BB mount -t ext4 -o rw /.secondrom/media/.secondrom/system.img /system
+$BB mount -t ext4 -o rw /.secondrom/system.img /system
 elif $BB [ "$FILESYSTEM" == "primary" ] ; then
 if $BB [ ! -d /sys/dev/block/179:22 ] ; then
         $BB mount -t ext4 -o rw /dev/block/mmcblk0p22 /system
